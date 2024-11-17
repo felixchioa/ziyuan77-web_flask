3aeb6a92315a975fcfa445a2159a6a057cbd6a01
趙峰 <feng.zhao1@pactera.com>
2024-11-17 14:16:04 +0800

    Refactor main.py to remove unused logger and update app.run() parameters

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from app import create_app
+from app.logger import Logger
+
+logger = Logger('routes')
 
 if __name__ == '__main__':
+    logger.info("Starting app")
     app = create_app()
     app.run(host='0.0.0.0', port=8080)
