4b6c35a07ae355fe27be5c6a6b3b51625ea788f7
趙峰 <feng.zhao1@pactera.com>
2024-11-17 11:26:50 +0800

    Update port number in main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     app = create_app()
     logger = Logger('run')
     logger.info("Starting app")
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
